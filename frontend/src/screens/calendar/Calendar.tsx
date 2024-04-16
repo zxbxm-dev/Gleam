12ee98feca9ec7793aaf3e1c3543887473637b3e
minseok9 <minseok7746@naver.com>
2024-04-16 16:56:28 +0900

    달력 레이아웃 생성

--- frontend/src/screens/calendar/Calendar.tsx
+++ frontend/src/screens/calendar/Calendar.tsx
@@ -1,6 +1,10 @@
 import "./Calendar.scss";
 import { Link } from "react-router-dom";
 
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+
 const Calendar = () => {
 
   return (
@@ -11,7 +15,39 @@ const Calendar = () => {
       
       <div className="content_container">
         <div className="calendar_container">
-          
+          <FullCalendar
+            plugins={[ dayGridPlugin ]}
+            initialView="dayGridMonth"
+            height="100%"
+            headerToolbar={{
+              start: 'prev title next',
+              center: '',
+              end: '',
+            }} // 달력 상단 헤더
+            dayHeaderFormat={{
+              weekday: 'long'
+            }} // 요일 포맷 변경
+            titleFormat={(date) => {
+              const year = date.date.year;
+              const month = date.date.month + 1;
+              return `${year}년 ${month}월`;
+            }} // 헤더 년월 포맷 변경
+            dayCellContent={(info)=> {
+              var number = document.createElement("a");
+              number.classList.add("fc-daygrid-day-number");
+              number.innerHTML = info.dayNumberText.replace("일","");
+              if(info.view.type === "dayGridMonth") {
+                return {
+                  html: number.outerHTML
+                };
+              }
+              return {
+                domNodes: []
+              };
+            }} // 날짜에 '일' 제거
+            locale='kr' // 한국어
+            fixedWeekCount={false} // 5주 표기
+          />
         </div>  
       </div>  
     </div>
