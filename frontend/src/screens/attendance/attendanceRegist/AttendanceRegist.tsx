07cec429ec0f25e61c77a6d3f2f55c46ec075779
minseok9 <minseok7746@naver.com>
2024-04-19 10:01:58 +0900

    출근부 테이블 구현

--- frontend/src/screens/attendance/attendanceRegist/AttendanceRegist.tsx
+++ frontend/src/screens/attendance/attendanceRegist/AttendanceRegist.tsx
@@ -1,27 +1,96 @@
+import React, { useState } from "react";
 import "./AttendanceRegist.scss";
 import { Link } from "react-router-dom";
+import { Select } from '@chakra-ui/react';
+import { Input } from '@chakra-ui/react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure } from '@chakra-ui/react'
 
 const AttendanceRegist = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+  const [selectedDateInfo, setSelectedDateInfo] = useState<{
+    year: number | null;
+    month: number | null;
+    date: number | null;
+    dayOfWeek: string | null;
+  }>({
+    year: null,
+    month: null,
+    date: null,
+    dayOfWeek: null
+  });
 
-  const generateDivs = () => {
-    const divs = [];
-    for (let i = 0; i < 30; i++) {
-      for (let j = 0; j < 31; j++) {
-        divs.push(<div className="conta" key={`${i}-${j}`}>&nbsp;</div>);
-      }
+  // 전체 연도의 정보
+  const yearData = [];
+
+  // 1월 ~ 12월 정보
+  for (let month = 0; month < 12; month++) {
+    const currentYear = selectedYear;
+
+    // 월별 날짜 계산
+    const firstDayOfMonth = new Date(currentYear, month, 1);
+    const lastDayOfMonth = new Date(currentYear, month + 1, 0);
+
+    // 월 일수 계산
+    const numberOfDaysInMonth = lastDayOfMonth.getDate();
+
+    // 첫 날 요일 계산
+    const firstDayOfWeek = firstDayOfMonth.getDay();
+
+    // 월 정보 저장
+    yearData.push({
+      month: month + 1,
+      numberOfDaysInMonth,
+      firstDayOfWeek
+    });
+  }
+
+  const DateDivs = (numberOfDaysInMonth: number, firstDayOfWeek: number) => {
+    const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+    const tableRows = [];
+    for (let j = 0; j < numberOfDaysInMonth; j++) {
+      const dayOfWeek = daysOfWeek[(firstDayOfWeek + j) % 7];
+      tableRows.push(
+        <tr className="dateconta" key={`${j}`}>
+          <td className="date">{j + 1}일<br/>{dayOfWeek}요일</td>
+        </tr>
+      );
     }
-    return divs;
+    return (
+      <table className="dateTable">
+        <tbody>
+          {tableRows}
+        </tbody>
+      </table>
+    );
   };
 
-  const DateDivs = () => {
-    const daysOfWeek = ["월", "화", "수", "목", "금", "토", "일"];
-    const divs = [];
-    for (let j = 0; j < 31; j++) {
-        const dayOfWeek = daysOfWeek[(j + 4) % 7];
-        divs.push(<div className="dateconta" key={`${j}`}>{j + 1}일<br/>{dayOfWeek}요일</div>);
+  const generateDivs = (numberOfDaysInMonth: number, year: number, month: number) => {
+    const tableRows = [];
+    for (let i = 0; i < numberOfDaysInMonth; i++) {
+      const rowCells = [];
+      for (let j = 0; j < 54; j++) {
+        const date = j + 1;
+        rowCells.push(
+          <td 
+            className="conta" 
+            key={`${i}-${j}`} 
+            onClick={() => handleDivClick(date, year, month)}
+          >
+            &nbsp;
+          </td>
+        );
+      }
+      tableRows.push(<td className="sconta" key={i}>{rowCells}</td>);
     }
-    return divs;
+    return (
+      <table className="table">
+        <tbody>
+          {tableRows}
+        </tbody>
+      </table>
+    );
   };
 
   const months = [
@@ -39,32 +108,172 @@ const AttendanceRegist = () => {
     { name: '12월', key: 'december' },
   ];
 
+  // 년도 변경
+  const handleYearChange = (event: any) => {
+    setSelectedYear(parseInt(event.target.value));
+  };
+
+  const handleDivClick = (date: number, year: number, month: number) => {
+    onOpen();
+    const dayOfWeekNames = ["일", "월", "화", "수", "목", "금", "토"];
+    const dayOfWeekIndex = new Date(year, month - 1, date).getDay(); // 0(일요일)부터 시작하는 요일 인덱스
+    const dayOfWeek = dayOfWeekNames[dayOfWeekIndex];
+    setSelectedDateInfo({
+      year: year,
+      month: month,
+      date: date,
+      dayOfWeek: dayOfWeek
+    });
+    // 여기서 Modal에 해당 날짜 정보를 전달하거나 처리할 수 있습니다.
+  };
+
   return (
     <div className="content">
       <div className="content_header">
         <div className="main_header">근태관리</div>
         <div className="main_header">＞</div>
         <Link to={"/attendance-regist"} className="sub_header">출근부</Link>
+        <Select width='120px' value={selectedYear} onChange={handleYearChange} position='absolute' top='5px' right='20px' bg='#EEEEEE'>
+          <option value={2022}>2022년</option>
+          <option value={2023}>2023년</option>
+          <option value={2024}>2024년</option>
+        </Select>
       </div>
       
       <div className="content_container">
         <Tabs variant='enclosed'>
           <TabList>
-            {months.map(month => (
-              <Tab key={month.key} _selected={{bg: '#FFFFFF', fontFamily: 'var(--font-family-Noto-B)'}} width='136.7px' bg='#DEDEDE' borderTop='1px solid #DEDEDE' borderRight='1px solid #DEDEDE' borderLeft='1px solid #DEDEDE' fontFamily='var(--font-family-Noto-R)'>{month.name}</Tab>
+            {yearData.map(monthData => (
+              <Tab key={monthData.month} _selected={{bg: '#FFFFFF', fontFamily: 'var(--font-family-Noto-B)'}} width='10%' bg='#DEDEDE' borderTop='1px solid #DEDEDE' borderRight='1px solid #DEDEDE' borderLeft='1px solid #DEDEDE' fontFamily='var(--font-family-Noto-R)'>{months[monthData.month - 1].name}</Tab>
             ))}
           </TabList>
 
           <TabPanels bg='white' height='790px' border='1px solid #DEDEDE' borderBottomRadius='10px'>
-            {months.map(month => (
-              <TabPanel key={month.key} className="container_attendance">
-                {DateDivs()}
-                {generateDivs()}
+            {yearData.map(monthData => (
+              <TabPanel key={monthData.month} className="container_attendance">
+                <div className="Excel">
+                <table className="Explan">
+                  <tbody>
+                    <tr>
+                      <td className="TopS" colSpan={2}>부서</td>
+                      <td className="TopS">성명</td>
+                    </tr>
+                    <tr>
+                      <td rowSpan={3}>블록체인<br/>사업부</td>
+                      <td rowSpan={3}>블록체인<br/>1팀</td>
+                      <td>권상원</td>
+                    </tr>
+                    <tr>
+                      <td>김도환</td>
+                    </tr>
+                    <tr>
+                      <td>권준우</td>
+                    </tr>
+                    <tr>
+                      <td rowSpan={6}>개발부</td>
+                      <td rowSpan={4}>개발 1팀</td>
+                      <td>진유빈</td>
+                    </tr>
+                    <tr>
+                      <td>장현지</td>
+                    </tr>
+                    <tr>
+                      <td>권채림</td>
+                    </tr>
+                    <tr>
+                      <td>구민석</td>
+                    </tr>
+                    <tr>
+                      <td rowSpan={2}>개발 2팀</td>
+                      <td>변도일</td>
+                    </tr>
+                    <tr>
+                      <td>이로운</td>
+                    </tr>
+                    <tr>
+                      <td rowSpan={4}>마케팅부</td>
+                      <td rowSpan={2}>디자인팀</td>
+                      <td>김현지</td>
+                    </tr>
+                    <tr>
+                      <td>서주희</td>
+                    </tr>
+                    <tr>
+                      <td rowSpan={2}>기획팀</td>
+                      <td>전아름</td>
+                    </tr>
+                    <tr>
+                      <td>함다슬</td>
+                    </tr>
+                    <tr>
+                      <td rowSpan={5}>관리부</td>
+                      <td rowSpan={3}>관리팀</td>
+                      <td>김효은</td>
+                    </tr>
+                    <tr>
+                      <td>우현지</td>
+                    </tr>
+                    <tr>
+                      <td>염승희</td>
+                    </tr>
+                    <tr>
+                      <td rowSpan={2}>지원팀</td>
+                      <td>김태희</td>
+                    </tr>
+                    <tr>
+                      <td>홍길동</td>
+                    </tr>
+                  
+                  </tbody>
+                </table>
+                <div>
+                  {DateDivs(monthData.numberOfDaysInMonth, monthData.firstDayOfWeek)}
+                  {generateDivs(monthData.numberOfDaysInMonth, selectedYear, monthData.month)}
+                </div>
+                </div>
               </TabPanel>
             ))}
           </TabPanels>
         </Tabs>
       </div>  
+      <Modal isOpen={isOpen} onClose={onClose} size='lg'>
+        <ModalOverlay />
+        <ModalContent height='400px' bg='#D9D9D9'>
+          <ModalHeader>
+            <span>{selectedDateInfo.year}.</span>
+            <span>{selectedDateInfo.month}.</span>
+            <span>{selectedDateInfo.date}.</span>
+            <span>{selectedDateInfo.dayOfWeek}</span>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody fontSize='30px' className="modal_content">
+            <div className="modal_input">
+              <div>출근시간 :</div>
+              <Input htmlSize={4} width='auto' borderColor='black'/>
+            </div>
+            <div className="modal_input">
+              <div>퇴근시간 :</div>
+              <Input htmlSize={4} width='auto' borderColor='black'/>
+            </div>
+            <div className="modal_input">
+              <div>기타 값 :</div>
+              <Select width='200px' value={selectedYear} onChange={handleYearChange} borderColor='black'>
+                <option value=''>반차</option>
+                <option value=''>당일반차</option> 
+                <option value=''>연차</option>
+                <option value=''>재택</option>
+                <option value=''>서울출근</option>
+                <option value=''>입사</option>
+              </Select>
+            </div>
+          </ModalBody>
+
+          <ModalFooter gap='10px'>
+            <button className="add_button">등록</button>
+            <button className="cancle_button">취소</button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 };
