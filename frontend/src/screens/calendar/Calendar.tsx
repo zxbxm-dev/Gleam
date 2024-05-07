660861a479616e283a6159ea2c7f137222452d71
HYEONJI1209 <hyeonji1209@naver.com>
2024-05-07 12:42:04 +0900

    calender datepicker 추가

--- frontend/src/screens/calendar/Calendar.tsx
+++ frontend/src/screens/calendar/Calendar.tsx
@@ -1,6 +1,6 @@
 import "./Calendar.scss";
 import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 
@@ -15,22 +15,25 @@ import {
   useDisclosure,
 } from '@chakra-ui/react';
 import { Select, Textarea, Input } from '@chakra-ui/react';
-
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const Calendar = () => {
   const { isOpen: isAddModalOpen, onOpen: onAddModalOpen, onClose: isAddModalClose } = useDisclosure();
   const { isOpen: isViewModalOpen, onOpen: onViewModalOpen, onClose: onViewModalClose } = useDisclosure();
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
 
   const events = [
-    { title: '구민석 반차', start: new Date('2024-4-17'), backgroundColor: '#ABF0FF', borderColor: '#ABF0FF' , textColor: '#000'},
-    { title: '구민석 연차', start: new Date('2024-4-17'), backgroundColor: '#7AE1A9', borderColor: '#7AE1A9' , textColor: '#000'},
-    { title: '구민석 외근', start: new Date('2024-4-17'), backgroundColor: '#D6CDC2', borderColor: '#D6CDC2' , textColor: '#000'},
-    { title: '구민석 워크숍', start: new Date('2024-4-17'), backgroundColor: '#FFD8B5', borderColor: '#FFD8B5' , textColor: '#000'},
-    { title: '구민석 출장', start: new Date('2024-4-17'), backgroundColor: '#B1C3FF', borderColor: '#B1C3FF' , textColor: '#000'},
-  	{ title: '구민석 연차', start: new Date('2024-4-18'), backgroundColor: '#7AE1A9', borderColor: '#7AE1A9' , textColor: '#000'},
-  	{ title: '구민석 연차', start: new Date('2024-4-18'), backgroundColor: '#7AE1A9', borderColor: '#7AE1A9' , textColor: '#000'},
-  	{ title: '구민석 연차', start: new Date('2024-4-18'), backgroundColor: '#7AE1A9', borderColor: '#7AE1A9' , textColor: '#000'},
-  	{ title: '구민석 연차', start: new Date('2024-4-18'), backgroundColor: '#7AE1A9', borderColor: '#7AE1A9' , textColor: '#000'},
+    { title: '구민석 연차', start: new Date('2024-05-17'), end: new Date('2024-05-18'), backgroundColor: '#ABF0FF', borderColor: '#ABF0FF', textColor: '#000' },
+    // { title: '구민석 연차', start: new Date('2024-5-18'), backgroundColor: '#7AE1A9', borderColor: '#7AE1A9' , textColor: '#000'},
+    { title: '구민석 외근', start: new Date('2024-4-17'), backgroundColor: '#D6CDC2', borderColor: '#D6CDC2', textColor: '#000' },
+    { title: '구민석 워크숍', start: new Date('2024-4-17'), backgroundColor: '#FFD8B5', borderColor: '#FFD8B5', textColor: '#000' },
+    { title: '구민석 출장', start: new Date('2024-4-17'), backgroundColor: '#B1C3FF', borderColor: '#B1C3FF', textColor: '#000' },
+    { title: '구민석 연차', start: new Date('2024-4-18'), backgroundColor: '#7AE1A9', borderColor: '#7AE1A9', textColor: '#000' },
+    { title: '구민석 연차', start: new Date('2024-4-18'), backgroundColor: '#7AE1A9', borderColor: '#7AE1A9', textColor: '#000' },
+    { title: '구민석 연차', start: new Date('2024-4-18'), backgroundColor: '#7AE1A9', borderColor: '#7AE1A9', textColor: '#000' },
+    { title: '구민석 연차', start: new Date('2024-4-18'), backgroundColor: '#7AE1A9', borderColor: '#7AE1A9', textColor: '#000' },
   ]
 
   return (
@@ -38,17 +41,17 @@ const Calendar = () => {
       <div className="content_header">
         <Link to={"/calendar"} className="sub_header">휴가관리</Link>
       </div>
-      
+
       <div className="content_container">
         <div className="calendar_container">
           <FullCalendar
-            plugins={[ dayGridPlugin ]}
+            plugins={[dayGridPlugin]}
             initialView="dayGridMonth"
             height="100%"
             customButtons={{
               Addschedule: {
                 text: '일정 추가　+',
-                click: function() {
+                click: function () {
                   onAddModalOpen();
                 },
               },
@@ -66,11 +69,11 @@ const Calendar = () => {
               const month = date.date.month + 1;
               return `${year}년 ${month}월`;
             }} // 헤더 년월 포맷 변경
-            dayCellContent={(info)=> {
+            dayCellContent={(info) => {
               var number = document.createElement("a");
               number.classList.add("fc-daygrid-day-number");
-              number.innerHTML = info.dayNumberText.replace("일","");
-              if(info.view.type === "dayGridMonth") {
+              number.innerHTML = info.dayNumberText.replace("일", "");
+              if (info.view.type === "dayGridMonth") {
                 return {
                   html: number.outerHTML
                 };
@@ -95,15 +98,15 @@ const Calendar = () => {
             eventClick={onViewModalOpen}
             moreLinkText='개 일정 더보기'
           />
-        </div>  
-      </div>  
+        </div>
+      </div>
       <Modal isOpen={isAddModalOpen} onClose={isAddModalClose} size='xl' isCentered={true}>
         <ModalOverlay />
         <ModalContent height='40vh' borderRadius='10px'>
-          <ModalHeader height='6vh' color='white' bg='#746E58' border='0' fontFamily= 'var(--font-family-Noto-B)' borderTopRadius='10px'>일정 등록하기</ModalHeader>
+          <ModalHeader height='6vh' color='white' bg='#746E58' border='0' fontFamily='var(--font-family-Noto-B)' borderTopRadius='10px'>일정 등록하기</ModalHeader>
           <ModalCloseButton />
           <ModalBody display='flex' flexDirection='column' gap='15px' fontFamily='var(--font-family-Noto-M)'>
-            <div style={{display: 'flex', gap: '10px'}}>
+            <div style={{ display: 'flex', gap: '10px' }}>
               <div>
                 <Select size='sm' width='4vw' borderRadius='5px' fontFamily='var(--font-family-Noto-M)'>
                   <option value='반차' style={{ color: '#FFB800' }}>반차</option>
@@ -117,20 +120,39 @@ const Calendar = () => {
                 <Input size='sm' width='22vw' placeholder='ex) OOO 반차' />
               </div>
             </div>
-            <div style={{display: 'flex', gap: '10px'}}>
-              <div style={{width: '4vw', textAlign: 'right'}}>
+            <div style={{ display: 'flex', gap: '10px' }}>
+              <div style={{ width: '4vw', textAlign: 'right' }}>
                 기간
               </div>
-              <div>
-
+              <div style={{display:'flex', width:'80%'}}>
+                <DatePicker
+                  selected={startDate}
+                  onChange={date => setStartDate(date)}
+                  selectsStart
+                  startDate={startDate}
+                  endDate={endDate}
+                  placeholderText="Start Date"
+                  dateFormat="yyyy-MM-dd"
+                />
+                <span style={{ margin: '0 5px' }}>~</span>
+                <DatePicker
+                  selected={endDate}
+                  onChange={date => setEndDate(date)}
+                  selectsEnd
+                  startDate={startDate}
+                  endDate={endDate}
+                  minDate={startDate}
+                  placeholderText="End Date"
+                  dateFormat="yyyy-MM-dd"
+                />
               </div>
             </div>
-            <div style={{display: 'flex', gap: '10px'}}>
-              <div style={{width: '4vw', textAlign: 'right'}}>
+            <div style={{ display: 'flex', gap: '10px' }}>
+              <div style={{ width: '4vw', textAlign: 'right' }}>
                 메모
               </div>
               <div>
-                <Textarea placeholder='내용을 입력해주세요.' size='sm' width='22vw' height='15vh' fontFamily='var(--font-family-Noto-R)'/>
+                <Textarea placeholder='내용을 입력해주세요.' size='sm' width='22vw' height='15vh' fontFamily='var(--font-family-Noto-R)' />
               </div>
             </div>
           </ModalBody>
@@ -145,7 +167,7 @@ const Calendar = () => {
       <Modal isOpen={isViewModalOpen} onClose={onViewModalClose} size='xl' isCentered={true}>
         <ModalOverlay />
         <ModalContent height='400px' borderRadius='10px'>
-          <ModalHeader height='55px' color='white' bg='#746E58' border='0' fontFamily= 'var(--font-family-Noto-B)' borderTopRadius='10px'>일정 확인</ModalHeader>
+          <ModalHeader height='55px' color='white' bg='#746E58' border='0' fontFamily='var(--font-family-Noto-B)' borderTopRadius='10px'>일정 확인</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
             <div>
