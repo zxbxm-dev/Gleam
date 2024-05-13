5ced59c0206178622988f2ea533863c750c57469
minseok9 <minseok7746@naver.com>
2024-05-13 10:17:59 +0900

    프로필수정 버튼 추가

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,10 +1,11 @@
 import "./Header.scss";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import {
   SideMenu,
   Logo,
   UserIcon,
   MenuArrow_down,
+  SettingIcon,
 } from "../../assets/images/index";
 import {
   Popover,
@@ -19,6 +20,7 @@ import { useRecoilState } from 'recoil';
 import { isSidebarVisibleState, userState } from '../../recoil/atoms';
 
 const Header = () => {
+  let navigate = useNavigate();
   const [isSidebarVisible, setIsSidebarVisible] = useRecoilState(isSidebarVisibleState);
   const [userInfo] = useRecoilState(userState);
   
@@ -54,7 +56,7 @@ const Header = () => {
             </PopoverTrigger>
             <Portal>
               <PopoverContent width='400px' height='200px' border='0' borderRadius='1px' marginTop='10px' marginRight='10px'>
-                <PopoverHeader color='white' bg='#746E58' border='0' fontFamily= 'var(--font-family-Noto-B)'>{userInfo.department}</PopoverHeader>
+                <PopoverHeader height='34px' color='white' bg='#746E58' border='0' fontFamily= 'var(--font-family-Noto-B)' fontSize='14px'>{userInfo.department}</PopoverHeader>
                 <PopoverCloseButton color='white' />
                 <PopoverBody display='flex' flexDirection='row' alignItems='center'>
                   <div style={{width: '140px', height: '150px', display: 'flex', flexDirection: 'column', alignItems: 'center' ,justifyContent: 'center'}}>
@@ -65,8 +67,11 @@ const Header = () => {
                   <div style={{width: '300px', height: '150px', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
                     <div style={{fontSize: '14px', color: '#909090', fontFamily: 'var(--font-family-Noto-M)'}}>연락처</div>
                     <div style={{fontSize: '16px', fontFamily: 'var(--font-family-Noto-M)'}}>010-0000-0000</div>
-                    <div style={{fontSize: '14px', color: '#909090', fontFamily: 'var(--font-family-Noto-M)', marginTop: '20px'}}>이메일</div>
+                    <div style={{fontSize: '14px', color: '#909090', fontFamily: 'var(--font-family-Noto-M)', marginTop: '20px'}}>메일주소</div>
                     <div style={{fontSize: '16px', fontFamily: 'var(--font-family-Noto-M)'}}>OOOO123456@four-chains.com</div>
+                  </div>
+                  <div style={{position: 'absolute', top: '50px', right: '20px', cursor: 'pointer'}} onClick={() => navigate('/editres')}>
+                    <img src={SettingIcon} alt="SettingIcon"/>
                   </div>
                 </PopoverBody>
               </PopoverContent>
