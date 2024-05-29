4d3f1574ad2344f5fa67d99bd7945d2b47b6d4fa
minseok9 <minseok7746@naver.com>
2024-05-29 16:38:32 +0900

    근태관리: api 연결

--- frontend/src/services/attendance/AttendanceServices.jsx
+++ frontend/src/services/attendance/AttendanceServices.jsx
@@ -1,8 +1,36 @@
 import api from "../../api/auth";
 
-// 출근부 데이터 전송
-const AttendRegist = (formData) => {
-  return api.post("/AttendRegist", formData);
+// 연차관리
+
+// 연차관리 데이터 조회
+const CheckAnnual = () => {
+  return api.get("/checkAnnual");
 }
 
-export { AttendRegist };
+// 연차관리 데이터 수정
+const EditAnnual = (annual_id) => {
+  return api.put(`/editAnnual/${annual_id}`);
+}
+
+
+
+
+// 출근부
+
+// 출근부 데이터 조회
+const CheckAttendance = () => {
+  return api.get("/checkAttendance");
+}
+
+// 출근부 데이터 작성
+const WriteAttendance = (formData) => {
+  return api.post("/writeAttendance", formData);
+}
+
+// 출근부 데이터 수정
+const EditAttendance = (attend_id) => {
+  return api.put(`/editAttendance/${attend_id}`)
+}
+
+
+export { CheckAnnual, EditAnnual, CheckAttendance, WriteAttendance, EditAttendance };
