b9a672b08836bb04e44c799c10afda24102b3fd8
minseok9 <minseok7746@naver.com>
2024-05-30 16:01:26 +0900

    인사정보관리: api 연결

---
+++ frontend/src/services/humanresource/HumanResourceServices.jsx
@@ -0,0 +1,40 @@
+import api from "../../api/auth";
+
+// 인사정보관리 조회
+const CheckHrInfo = (user_id) => {
+  return api.get(`/checkHrInfo/${user_id}`);
+}
+
+// 인사정보관리 제출
+const WriteHrInfo = (formData) => {
+  return api.post("writeHrInfo", formData);
+}
+
+// 인사정보관리 수정
+const EditHrInfo = (hrinfo_id) => {
+  return api.put(`/editHrInfo/${hrinfo_id}`);
+}
+
+
+
+// 인사이동 조희
+const CheckAppointment = (user_id) => {
+  return api.get(`/checkAppointment/${user_id}`);
+}
+
+// 인사이동 등록
+const writeAppointment = (formData) => {
+  return api.post("/writeAppointment", formData);
+}
+
+// 인사이동 수정
+const EditAppointment = (appoint_id, formData) => {
+  return api.put(`/editAppointment/${appoint_id}`, formData);
+}
+
+// 인사이동 삭제
+const DeleteAppointment = (appoint_id) => {
+  return api.delete(`/deleteAppointment/${appoint_id}`);
+}
+
+export { CheckHrInfo, WriteHrInfo, EditHrInfo, CheckAppointment, writeAppointment, EditAppointment, DeleteAppointment };
