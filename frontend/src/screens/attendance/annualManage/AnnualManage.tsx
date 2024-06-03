6891eaa7ad4afdc7d52a9a7dc9f53f4e4fcfb874
HYEONJI1209 <hyeonji1209@naver.com>
2024-06-03 14:30:19 +0900

    pdf,,,,,조금 더 수정 필요

--- frontend/src/screens/attendance/annualManage/AnnualManage.tsx
+++ frontend/src/screens/attendance/annualManage/AnnualManage.tsx
@@ -31,8 +31,8 @@ const AnnualManage = () => {
       element.style.width = element.scrollWidth + 'px';
       html2canvas(element).then(canvas => {
         const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF('p', 'mm', 'a4');
-        const imgWidth = 210; // A4 크기에서 이미지 너비
+        const pdf = new jsPDF('l', 'mm', 'a4');
+        const imgWidth = 297; // A4 크기에서 이미지 너비
         const imgHeight = (canvas.height * imgWidth) / canvas.width; // 이미지의 원래 높이에 따른 비율에 따라 조정
         pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
         pdf.save('연차관리.pdf');
