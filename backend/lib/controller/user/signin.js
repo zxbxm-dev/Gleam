d1dc62d74aa2da613d8754ff5cd70984e987a70e
yupp95 <jinyubin1933@naver.com>
2024-06-05 11:40:52 +0900

    회원 관리

--- backend/lib/controller/user/signin.js
+++ backend/lib/controller/user/signin.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 const secretKey = process.env.DB_DATABASE
 
+//로그인
 const login = async (req, res) => {
   const { username, password } = req.body;
 
@@ -15,12 +16,12 @@ const login = async (req, res) => {
     if (user) {
       const match = await bcrypt.compare(password, user.password);
       if (match) {
-        // JWT 토큰 생성
+        // JWT 토큰 생성 (1시간)
         const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: "1h" });
 
         // 사용자 정보 응답에 포함
         res.status(200).json({ 
-          message: "로그인 성공",
+          success: "로그인 성공",
           token,
           user: {
             username: user.username,
@@ -37,15 +38,48 @@ const login = async (req, res) => {
           }
         });
       } else {
-        res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
+        res.status(401).json({ error: "비밀번호가 일치하지 않습니다." });
       }
     } else {
-      res.status(404).json({ message: "사용자를 찾을 수 없습니다." });
+      res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "서버 오류" });
+    res.status(500).json({ message: "로그인 서버 오류", error});
   }
 };
 
-module.exports = { login };
+// 아이디 찾기
+const findUsername = async (req, res) => {
+  const { username, spot, phoneNumber } = req.body;
+
+  try {
+    const user = await User.findOne({ 
+      where: { 
+        username: username,
+        spot: spot,
+        phoneNumber: phoneNumber
+      } 
+    });
+
+    if (user) {
+      // 사용자를 찾은 경우
+      res.status(200).json({ 
+        success: "사용자를 찾았습니다.",
+        username: user.username,
+        userId: user.userId,
+      });
+    } else {
+      // 사용자를 찾지 못한 경우
+      res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "아이디 찾기 서버 오류" });
+  }
+};
+
+module.exports = { 
+  login,
+  findUsername
+};
