13f7dc6281b4151c58b1f963ff4d7b22b1f1aedc
HYEONJI1209 <hyeonji1209@naver.com>
2024-06-21 17:07:54 +0900

    코드정리

--- frontend/src/screens/mail/Mail.tsx
+++ frontend/src/screens/mail/Mail.tsx
@@ -309,60 +309,60 @@ const Mail = () => {
       </div>
 
       <div className="mail_pagination">
-  {filteredMails.length > itemsPerPage && (
-    <div className="Pagination">
-      <img
-        src={LeftIcon}
-        onClick={() => {
-          if (page > 1) {
-            setPage(page - 1);
-            setCurrentPage(page - 1);
-          }
-        }}
-        alt="Previous Page"
-      />
-      <input
-        type="text"
-        value={page}
-        className="PageInput"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          const inputPage = e.target.value.replace(/\D/g, '');
-          setPage(inputPage ? Number(inputPage) : 0);
-        }}
-        onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
-          if (e.key === 'Enter') {
-            const inputPage = Number(e.currentTarget.value);
-            if (!isNaN(inputPage) && inputPage >= 1 && inputPage <= totalPages) {
-              setCurrentPage(inputPage);
-            } else {
-              setPage(1);
-            }
-          }
-        }}
-        onBlur={(e: React.FocusEvent<HTMLInputElement>) => {
-          const inputPage = Number(e.currentTarget.value);
-          if (!isNaN(inputPage) && inputPage >= 1 && inputPage <= totalPages) {
-            setCurrentPage(inputPage);
-          } else {
-            setPage(1);
-          }
-        }}
-      />
-      <span> / </span>
-      <span className="PagesTotal">{totalPages}</span>
-      <img
-        src={RightIcon}
-        onClick={() => {
-          if (page < totalPages) {
-            setPage(page + 1);
-            setCurrentPage(page + 1);
-          }
-        }}
-        alt="Next Page"
-      />
-    </div>
-  )}
-</div>
+        {filteredMails.length > itemsPerPage && (
+          <div className="Pagination">
+            <img
+              src={LeftIcon}
+              onClick={() => {
+                if (page > 1) {
+                  setPage(page - 1);
+                  setCurrentPage(page - 1);
+                }
+              }}
+              alt="Previous Page"
+            />
+            <input
+              type="text"
+              value={page}
+              className="PageInput"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                const inputPage = e.target.value.replace(/\D/g, '');
+                setPage(inputPage ? Number(inputPage) : 0);
+              }}
+              onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                if (e.key === 'Enter') {
+                  const inputPage = Number(e.currentTarget.value);
+                  if (!isNaN(inputPage) && inputPage >= 1 && inputPage <= totalPages) {
+                    setCurrentPage(inputPage);
+                  } else {
+                    setPage(1);
+                  }
+                }
+              }}
+              onBlur={(e: React.FocusEvent<HTMLInputElement>) => {
+                const inputPage = Number(e.currentTarget.value);
+                if (!isNaN(inputPage) && inputPage >= 1 && inputPage <= totalPages) {
+                  setCurrentPage(inputPage);
+                } else {
+                  setPage(1);
+                }
+              }}
+            />
+            <span> / </span>
+            <span className="PagesTotal">{totalPages}</span>
+            <img
+              src={RightIcon}
+              onClick={() => {
+                if (page < totalPages) {
+                  setPage(page + 1);
+                  setCurrentPage(page + 1);
+                }
+              }}
+              alt="Next Page"
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
