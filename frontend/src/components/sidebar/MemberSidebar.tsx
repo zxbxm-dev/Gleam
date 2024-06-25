23757390fccf69bd082adaae1daf586ef6628a77
HYEONJI1209 <hyeonji1209@naver.com>
2024-06-25 16:09:11 +0900

    멤버 사이드바 완료

--- frontend/src/components/sidebar/MemberSidebar.tsx
+++ frontend/src/components/sidebar/MemberSidebar.tsx
@@ -1,9 +1,9 @@
 import "./Sidebar.scss";
 import React, { useState } from "react";
-import { SideUp } from "../../assets/images/index";
 import { userState } from '../../recoil/atoms';
 import { useRecoilState } from 'recoil';
 import { useLocation } from 'react-router-dom';
+import { SideUp } from "../../assets/images/index";
 
 type Member = [string, string, string, string, string, string, string[]];
 
@@ -11,104 +11,153 @@ interface Props {
   onClickMember: (name: string, dept: string, team: string, position: string) => void;
 }
 
+const membersData: Member[] = [
+  ['id1', 'pw1', '이정훈', '포체인스 주식회사', '', '대표', ['']],
+  ['id2', 'pw2', '안후상', '포체인스 주식회사', '', '이사', ['']],
+  ['id3', 'pw3', '진유빈', '개발부', '', '부서장', ['']],
+  ['id4', 'pw4', '장현지', '개발부', '개발1팀', '사원', ['']],
+  ['id5', 'pw5', '구민석', '개발부', '개발1팀', '사원', ['']],
+  ['id6', 'pw6', '장현지', '알고리즘 연구실', '암호 연구팀', '사원', ['']],
+  ['id7', 'pw7', '구민석', '알고리즘 연구실', '암호 연구팀', '사원', ['']],
+  ['id8', 'pw8', '진유빈', '알고리즘 연구실', 'AI 연구팀', '사원', ['']],
+  ['id9', 'pw9', '권준우', '알고리즘 연구실', 'AI 연구팀', '사원', ['']],
+  ['id10', 'pw10', '심민지', '알고리즘 연구실', '', '연구실장', ['']],
+];
+
+type CurrentPageType =
+  '포체인스주식회사' | '개발부' | '개발1팀' | '개발2팀' | '블록체인사업부' | '블록체인1팀' |
+  '마케팅부' | '기획팀' | '디자인팀' | '관리부' | '관리팀' | '지원팀' |
+  '시설팀' | 'R&D센터' | '알고리즘연구실' | '동형분석연구실' | '블록체인연구실';
+
 const MemberSidebar: React.FC<Props> = ({ onClickMember }) => {
   const [user, setUser] = useRecoilState(userState);
   const location = useLocation();
-
-  const members: Member[] = [
-    ['id1', 'pw1', '이정훈', '포체인스 주식회사', '', '대표', ['']],
-    ['id2', 'pw2', '안후상', '포체인스 주식회사', '', '이사', ['']],
-    ['id3', 'pw3', '진유빈', '개발부', '', '부서장', ['']],
-    ['id4', 'pw4', '장현지', '개발부', '개발1팀', '사원', ['']],
-    ['id5', 'pw5', '구민석', '개발부', '개발1팀', '사원', ['']],
-  ];
-
-  const [isExpanded, setIsExpanded] = useState(false);
-  const [currentPage, setCurrentPage] = useState<'Main' | 'Devpart' | 'Dev1' | 'Dev2'>('Main');
+  const [sidebarState, setSidebarState] = useState<{ isExpanded: boolean; currentPage: CurrentPageType }>({
+    isExpanded: false,
+    currentPage: '포체인스주식회사',
+  });
 
   const toggleSidebar = () => {
-    setIsExpanded(!isExpanded);
+    setSidebarState({ ...sidebarState, isExpanded: !sidebarState.isExpanded });
   };
 
-  const handleClickMain = () => {
-    setCurrentPage('Main');
+  const handleClick = (page: CurrentPageType) => {
+    setSidebarState({ ...sidebarState, currentPage: page });
   };
 
-  const handleClickDept = () => {
-    setCurrentPage('Devpart');
+  const { isExpanded, currentPage } = sidebarState;
+
+  const renderMembers = () => {
+    return (
+      <div className="MemberTab">
+        <div className="Fourchains">
+          <span onClick={() => handleClick('포체인스주식회사')}>&lt;</span>
+          <div>&nbsp;{currentPage}</div>
+        </div>
+
+        {membersData
+          .filter((member) => {
+            switch (currentPage) {
+              case '포체인스주식회사':
+                return member[3] === '포체인스 주식회사' && (member[5] === '대표' || member[5] === '이사');
+              case '개발부':
+                return member[3] === '개발부' && member[4] === '';
+              case '개발1팀':
+                return member[3] === '개발부' && member[4] === '개발1팀';
+              case '개발2팀':
+                return member[3] === '개발부' && member[4] === '개발2팀';
+              case '블록체인사업부':
+                return member[3] === '블록체인사업부' && member[4] === '';
+              case '블록체인1팀':
+                return member[3] === '블록체인사업부' && member[4] === '블록체인 1팀';
+              case '마케팅부':
+                return member[3] === '마케팅부' && member[4] === '';
+              case '기획팀':
+                return member[3] === '마케팅부' && member[4] === '기획팀';
+              case '디자인팀':
+                return member[3] === '마케팅부' && member[4] === '디자인팀';
+              case '관리부':
+                return member[3] === '관리부' && member[4] === '';
+              case '관리팀':
+                return member[3] === '관리부' && member[4] === '관리팀';
+              case '지원팀':
+                return member[3] === '관리부' && member[4] === '지원팀';
+              case '시설팀':
+                return member[3] === '관리부' && member[4] === '시설팀';
+              case 'R&D센터':
+                return member[3] === '알고리즘 연구실' && member[5] === '연구실장';
+              case '알고리즘연구실':
+                return member[3] === '알고리즘 연구실' && (member[4] === '' || member[4].includes('연구팀'));
+              case '동형분석연구실':
+                return member[3] === '동형분석 연구실' && (member[4] === '' || member[4] === '동형분석 연구팀');
+              case '블록체인연구실':
+                return member[3] === '블록체인 연구실' && (member[4] === '' || member[4] === 'AI 개발팀' || member[4] === '크립토 블록체인 연구팀');
+              default:
+                return false;
+            }
+          })
+          .map((member, index) => (
+            <div key={index} className="member">
+              <div>{member[2]} | {member[5]}</div>
+            </div>
+          ))}
+      </div>
+    );
   };
 
-  const handleClickDev1 = () => {
-    setCurrentPage('Dev1');
-  };
-
-  const handleClickDev2 = () => {
-    setCurrentPage('Dev2');
+  const renderDept = () => {
+    return (
+      <div className="DeptMain">
+        {currentPage === '포체인스주식회사' && (
+          <>
+            <div onClick={() => handleClick('개발부')}>개발부</div>
+            <div onClick={() => handleClick('블록체인사업부')}>블록체인사업부</div>
+            <div onClick={() => handleClick('마케팅부')}>마케팅부</div>
+            <div onClick={() => handleClick('관리부')}>관리부</div>
+            <div onClick={() => handleClick('R&D센터')}>R&D센터센터</div>
+          </>
+        )}
+        {currentPage === '개발부' && (
+          <>
+            <div onClick={() => handleClick('개발1팀')}>개발 1팀</div>
+            <div onClick={() => handleClick('개발2팀')}>개발 2팀</div>
+          </>
+        )}
+        {currentPage === '블록체인사업부' && (
+          <>
+            <div onClick={() => handleClick('블록체인1팀')}>블록체인 1팀</div>
+          </>
+        )}
+        {currentPage === '마케팅부' && (
+          <>
+            <div onClick={() => handleClick('기획팀')}>기획팀</div>
+            <div onClick={() => handleClick('디자인팀')}>디자인팀</div>
+          </>
+        )}
+        {currentPage === '관리부' && (
+          <>
+            <div onClick={() => handleClick('관리팀')}>관리팀</div>
+            <div onClick={() => handleClick('지원팀')}>지원팀</div>
+            <div onClick={() => handleClick('시설팀')}>시설팀</div>
+          </>
+        )}
+        {currentPage === 'R&D센터' && (
+          <>
+            <div onClick={() => handleClick('알고리즘연구실')}>알고리즘 연구실</div>
+            <div onClick={() => handleClick('동형분석연구실')}>동형분석 연구실</div>
+            <div onClick={() => handleClick('블록체인연구실')}>블록체인 연구실</div>
+          </>
+        )}
+      </div>
+    );
   };
 
   return (
     <div className={`memberSidebar ${isExpanded ? 'expanded' : ''}`}>
       <img src={SideUp} alt="Expand Sidebar" onClick={toggleSidebar} />
       <div className="memberDetails">
-        <div className="Fourchains" onClick={handleClickMain}>포체인스 주식회사</div>
-        {currentPage === 'Main' ?
-          <div className="MemberTab">
-            {members.slice(0, 2).map((member, index) => {
-              if (member[5] === '대표' || member[5] === '이사') {
-                return (
-                  <div key={index} className="member">
-                    <div>{member[2]} | {member[5]}</div>
-                  </div>
-                );
-              } else {
-                return null;
-              }
-            })}
-            <div className="DeptMain">
-              <div onClick={handleClickDept}>개발부</div>
-              <div onClick={handleClickDept}>블록체인사업부</div>
-              <div onClick={handleClickDept}>마케팅부</div>
-              <div onClick={handleClickDept}>관리부</div>
-              <div onClick={handleClickDept}>R&D센터</div>
-            </div>
-          </div>
-          : (currentPage === 'Devpart' ?
-            <div>
-              {members
-                .filter(member => member[3] === '개발부' && member[4] === '')
-                .map((member, index) => (
-                  <div key={index} className="member">
-                    <div>{member[2]} | {member[5]}</div>
-                  </div>
-                ))}
-              <div onClick={handleClickDev1}>개발 1팀</div>
-              <div>개발 2팀</div>
-            </div>
-            : (currentPage === 'Dev1' ?
-              <div>
-                {members
-                  .filter(member => member[3] === '개발부' && member[4] === '개발1팀')
-                  .map((member, index) => (
-                    <div key={index} className="member">
-                      <div>{member[2]} | {member[5]}</div>
-                    </div>
-                  ))}
-              </div>
-              : (currentPage === 'Dev2' ?
-                <div>
-                  {members
-                    .filter(member => member[3] === '개발부' && member[4] === '개발2팀')
-                    .map((member, index) => (
-                      <div key={index} className="member">
-                        <div>{member[2]} | {member[5]}</div>
-                      </div>
-                    ))}
-                </div>
-                : null
-              )
-            )
-          )
-        }
+        {renderMembers()}
+        {renderDept()}
       </div>
     </div>
   );
