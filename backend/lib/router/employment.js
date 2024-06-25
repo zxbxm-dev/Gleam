1fd8fe9665200b056ddb655f9352efb98778cd49
yupp95 <jinyubin1933@naver.com>
2024-06-25 14:01:35 +0900

    채용공고

--- backend/lib/router/employment.js
+++ backend/lib/router/employment.js
@@ -5,6 +5,9 @@ module.exports = (app) => {
     const router = express.Router();
   
     router.post('/writeEmploy', jobPostingConstroller.createJobPosting);
+    router.get('/checkEmploy', jobPostingConstroller.getAllJobPosting);
+    router.put('/editEmploy/:employ_id', jobPostingConstroller.editJobPosting);
+    router.delete('/deleteEmploy/:employ_id', jobPostingConstroller.deleteJobPosting);
 
     app.use("/api", router);
   };
