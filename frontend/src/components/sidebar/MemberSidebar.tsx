749ea51d1f271073fbe456209f2c7e9eefc314ff
HYEONJI1209 <hyeonji1209@naver.com>
2024-06-28 15:12:12 +0900

    css 수정

--- frontend/src/components/sidebar/MemberSidebar.tsx
+++ frontend/src/components/sidebar/MemberSidebar.tsx
@@ -1,25 +1,24 @@
 import "./Sidebar.scss";
-import React, { useState } from "react";
-import { SideUp, SideDown } from "../../assets/images/index";
+import React, { useState, useEffect } from "react";
+import { SideUp, SideDown, UserIcon_dark } from "../../assets/images/index";
+import { PersonData } from "../../services/person/PersonServices";
 
-type Member = [string, string, string, string, string, string, string[]];
+// Define interfaces for types
+interface Person {
+  userId: string;
+  username: string;
+  position: string;
+  department: string;
+  team: string;
+  phoneNumber?: string;
+  usermail?: string;
+  entering: Date;
+  attachment: string;
+}
 
 interface Props {
   onClickMember: (name: string, dept: string, team: string, position: string) => void;
 }
-
-const membersData: Member[] = [
-  ['id1', 'pw1', '이정훈', '포체인스 주식회사', '', '대표', ['']],
-  ['id2', 'pw2', '안후상', '포체인스 주식회사', '', '이사', ['']],
-  ['id3', 'pw3', '진유빈', '개발부', '', '부서장', ['']],
-  ['id4', 'pw4', '장현지', '개발부', '개발1팀', '사원', ['']],
-  ['id5', 'pw5', '구민석', '개발부', '개발1팀', '사원', ['']],
-  ['id6', 'pw6', '장현지', '알고리즘 연구실', '암호 연구팀', '사원', ['']],
-  ['id7', 'pw7', '구민석', '알고리즘 연구실', '암호 연구팀', '사원', ['']],
-  ['id8', 'pw8', '진유빈', '알고리즘 연구실', 'AI 연구팀', '사원', ['']],
-  ['id9', 'pw9', '권준우', '알고리즘 연구실', 'AI 연구팀', '사원', ['']],
-  ['id10', 'pw10', '심민지', '알고리즘 연구실', '', '연구실장', ['']],
-];
 
 type CurrentPageType =
   '포체인스주식회사' | '개발부' | '개발1팀' | '개발2팀' | '블록체인사업부' | '블록체인1팀' |
@@ -27,78 +26,98 @@ type CurrentPageType =
   '시설팀' | 'R&D센터' | '알고리즘연구실' | '동형분석연구실' | '블록체인연구실';
 
 const MemberSidebar: React.FC<Props> = ({ onClickMember }) => {
+  const [personData, setPersonData] = useState<Person[] | null>(null);
   const [sidebarState, setSidebarState] = useState<{ isExpanded: boolean; currentPage: CurrentPageType }>({
     isExpanded: true,
     currentPage: '포체인스주식회사',
   });
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await PersonData();
+        const sortedData = response.data.sort((a: any, b: any) => new Date(a.entering).getTime() - new Date(b.entering).getTime());
+        setPersonData(sortedData);
+      } catch (err) {
+        console.error("Error fetching person data:", err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const toggleSidebar = () => {
-    setSidebarState({ ...sidebarState, isExpanded: !sidebarState.isExpanded });
+    setSidebarState((prevState) => ({
+      ...prevState,
+      isExpanded: !prevState.isExpanded,
+    }));
   };
 
   const handleClick = (page: CurrentPageType) => {
     setSidebarState({ ...sidebarState, currentPage: page });
   };
 
-  const handleMemberClick = (member: Member) => {
-    const [name, dept, team, position] = member;
-    onClickMember(name, dept, team, position);
+  const handleMemberClick = (person: Person) => {
+    onClickMember(person.username, person.department, person.team, person.position);
   };
 
-  const { isExpanded, currentPage } = sidebarState;
-
   const renderMembers = () => {
+    if (!personData) return null;
+
     return (
       <div className="MemberTab">
         <div className="Fourchains">
           <span onClick={() => handleClick('포체인스주식회사')}>&lt; &nbsp;</span>
-          <div>{currentPage}</div>
+          <div>{sidebarState.currentPage}</div>
         </div>
 
-        {membersData
-          .filter((member) => {
-            switch (currentPage) {
+        {personData
+          .filter((person) => {
+            switch (sidebarState.currentPage) {
               case '포체인스주식회사':
-                return member[3] === '포체인스 주식회사' && (member[5] === '대표' || member[5] === '이사');
+                return person.department === '' && (person.position === '대표이사' || person.position === '이사');
               case '개발부':
-                return member[3] === '개발부' && member[4] === '';
+                return person.department === '개발부' && person.team === '';
               case '개발1팀':
-                return member[3] === '개발부' && member[4] === '개발1팀';
+                return person.department === '개발부' && person.team === '개발 1팀';
               case '개발2팀':
-                return member[3] === '개발부' && member[4] === '개발2팀';
+                return person.department === '개발부' && person.team === '개발 2팀';
               case '블록체인사업부':
-                return member[3] === '블록체인사업부' && member[4] === '';
+                return person.department === '블록체인사업부' && person.team === '';
               case '블록체인1팀':
-                return member[3] === '블록체인사업부' && member[4] === '블록체인 1팀';
+                return person.department === '블록체인사업부' && person.team === '블록체인 1팀';
               case '마케팅부':
-                return member[3] === '마케팅부' && member[4] === '';
+                return person.department === '마케팅부' && person.team === '';
               case '기획팀':
-                return member[3] === '마케팅부' && member[4] === '기획팀';
+                return person.department === '마케팅부' && person.team === '기획팀';
               case '디자인팀':
-                return member[3] === '마케팅부' && member[4] === '디자인팀';
+                return person.department === '마케팅부' && person.team === '디자인팀';
               case '관리부':
-                return member[3] === '관리부' && member[4] === '';
+                return person.department === '관리부' && person.team === '';
               case '관리팀':
-                return member[3] === '관리부' && member[4] === '관리팀';
+                return person.department === '관리부' && person.team === '관리팀';
               case '지원팀':
-                return member[3] === '관리부' && member[4] === '지원팀';
+                return person.department === '관리부' && person.team === '지원팀';
               case '시설팀':
-                return member[3] === '관리부' && member[4] === '시설팀';
+                return person.department === '관리부' && person.team === '시설팀';
               case 'R&D센터':
-                return member[3] === '알고리즘 연구실' && member[5] === '연구실장';
+                return person.department === '알고리즘 연구실' && person.position === '연구실장';
               case '알고리즘연구실':
-                return member[3] === '알고리즘 연구실' && (member[4] === '' || member[4].includes('연구팀'));
+                return person.department === '알고리즘 연구실' && (person.team === '' || person.team.includes('연구팀'));
               case '동형분석연구실':
-                return member[3] === '동형분석 연구실' && (member[4] === '' || member[4] === '동형분석 연구팀');
+                return person.department === '동형분석 연구실' && (person.team === '' || person.team === '동형분석 연구팀');
               case '블록체인연구실':
-                return member[3] === '블록체인 연구실' && (member[4] === '' || member[4] === 'AI 개발팀' || member[4] === '크립토 블록체인 연구팀');
+                return person.department === '블록체인 연구실' && (person.team === '' || person.team === 'AI 개발팀' || person.team === '크립토 블록체인 연구팀');
               default:
                 return false;
             }
           })
-          .map((member, index) => (
-            <div key={index} className="member" onClick={() => handleMemberClick(member)}>
-              <div>{member[2]} | {member[5]}</div>
+          .map((person, index) => (
+            <div key={index} className="member" onClick={() => handleMemberClick(person)}>
+              <div className="MemberFlex">
+                <img src={person.attachment ? person.attachment : UserIcon_dark} />
+                <div className="Font">{person.username} | {person.position}</div>
+              </div>
             </div>
           ))}
       </div>
@@ -108,7 +127,7 @@ const MemberSidebar: React.FC<Props> = ({ onClickMember }) => {
   const renderDept = () => {
     return (
       <div className="DeptMain">
-        {currentPage === '포체인스주식회사' && (
+        {sidebarState.currentPage === '포체인스주식회사' && (
           <>
             <div onClick={() => handleClick('개발부')}>개발부</div>
             <div onClick={() => handleClick('블록체인사업부')}>블록체인사업부</div>
@@ -117,31 +136,31 @@ const MemberSidebar: React.FC<Props> = ({ onClickMember }) => {
             <div onClick={() => handleClick('R&D센터')}>R&D</div>
           </>
         )}
-        {currentPage === '개발부' && (
+        {sidebarState.currentPage === '개발부' && (
           <>
             <div onClick={() => handleClick('개발1팀')}>개발 1팀</div>
             <div onClick={() => handleClick('개발2팀')}>개발 2팀</div>
           </>
         )}
-        {currentPage === '블록체인사업부' && (
+        {sidebarState.currentPage === '블록체인사업부' && (
           <>
             <div onClick={() => handleClick('블록체인1팀')}>블록체인 1팀</div>
           </>
         )}
-        {currentPage === '마케팅부' && (
+        {sidebarState.currentPage === '마케팅부' && (
           <>
             <div onClick={() => handleClick('기획팀')}>기획팀</div>
             <div onClick={() => handleClick('디자인팀')}>디자인팀</div>
           </>
         )}
-        {currentPage === '관리부' && (
+        {sidebarState.currentPage === '관리부' && (
           <>
             <div onClick={() => handleClick('관리팀')}>관리팀</div>
             <div onClick={() => handleClick('지원팀')}>지원팀</div>
             <div onClick={() => handleClick('시설팀')}>시설팀</div>
           </>
         )}
-        {currentPage === 'R&D센터' && (
+        {sidebarState.currentPage === 'R&D센터' && (
           <>
             <div onClick={() => handleClick('알고리즘연구실')}>알고리즘 연구실</div>
             <div onClick={() => handleClick('동형분석연구실')}>동형분석 연구실</div>
@@ -153,8 +172,8 @@ const MemberSidebar: React.FC<Props> = ({ onClickMember }) => {
   };
 
   return (
-    <div className={`memberSidebar ${isExpanded ? 'expanded' : ''}`}>
-      <img src={isExpanded ? SideDown : SideUp} alt="Expand Sidebar" onClick={toggleSidebar} />
+    <div className={`memberSidebar ${sidebarState.isExpanded ? 'expanded' : ''}`}>
+      <img src={sidebarState.isExpanded ? SideDown : SideUp} alt="Expand Sidebar" onClick={toggleSidebar} />
       <div className="memberDetails">
         {renderMembers()}
         {renderDept()}
