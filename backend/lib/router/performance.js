1e42f6b94af408ce483a614cd42c2f1da82a1e15
yupp95 <jinyubin1933@naver.com>
2024-07-08 14:09:25 +0900

    상세 조회 오류 수정

--- backend/lib/router/performance.js
+++ backend/lib/router/performance.js
@@ -4,13 +4,20 @@ module.exports = (app) => {
   const checkEvaluation = require("../controller/performance/check_Evaluation");
   const express = require("express");
   const router = express.Router();
-  const path = require('path');
+  const path = require("path");
 
+  // 인사평가 제출
   router.post("/writePerform", upload, evaluationController.submitEvaluation);
+  // 인사평가 목록 조회
   router.get("/checkPerform", checkEvaluation.getMyEvaluation);
+  // 특정 파일 상세 조회
+  router.get("/fileDetails/:filename", checkEvaluation.getFileDetails);
 
   // 정적 파일 서빙 설정
-  app.use("/uploads", express.static(path.join(__dirname, 'uploads', 'performanceFile')));
+  app.use(
+    "/uploads",
+    express.static(path.join(__dirname, "uploads", "performanceFile"))
+  );
 
   app.use("/api", router);
 };
