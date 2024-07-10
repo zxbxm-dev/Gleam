97c53ca1b04c624ce8a0fd8f0d2a126f2e04dbb8
yupp95 <jinyubin1933@naver.com>
2024-07-10 14:09:03 +0900

    출근부 수정

--- backend/lib/controller/attendance/officeHour.js
+++ backend/lib/controller/attendance/officeHour.js
@@ -1,11 +1,14 @@
 const models = require("../../models");
 const Attendance = models.Attendance;
-const csv = require("csv-parser");
-const iconv = require("iconv-lite");
-const fs = require("fs");
-const path = require("path");
+// const csv = require("csv-parser");
+// const iconv = require("iconv-lite");
+// const fs = require("fs");
+// const path = require("path");
+const path = require('path');
+const fs = require('fs');
+const xlsx = require('xlsx');
 
-// 출근부 조회
+// 출근부 조회 -----------------------------------------------------------------------------------------------------------
 const getAllAttendance = async (req, res) => {
   try {
     const events = await Attendance.findAll();
@@ -16,15 +19,150 @@ const getAllAttendance = async (req, res) => {
   }
 };
 
-// 출근부 작성 (CSV 파일 업로드 및 데이터베이스 저장)
+// 출근부 수정 -----------------------------------------------------------------------------------------------------------
+const updateAttendance = async (req, res) => {
+  try {
+    const { attend_id } = req.params;
+    const { updateData } = req.body;
+
+    if (!attend_id) {
+      return res.status(400).json({ error: "ID가 제공되지 않았습니다" });
+    }
+
+    const attendanceRecord = await Attendance.findByPk(id);
+    if (!attendanceRecord) {
+      return res.status(404).json({ error: "출근부 데이터를 찾을 수 없습니다" });
+    }
+
+    const existingDataList = attendanceRecord.DataList || [];
+    const updatedDataList = [
+      existingDataList[0], // 출근시간
+      existingDataList[1], // 퇴근시간
+      updateData || '' // '목록' 항목
+    ];
+
+    attendanceRecord.DataList = updatedDataList;
+    await attendanceRecord.save();
+
+    res.status(200).json({ message: "출근부 데이터가 성공적으로 업데이트되었습니다", data: attendanceRecord });
+  } catch (error) {
+    console.error("출근부 데이터 업데이트 중 오류 발생:", error);
+    res.status(500).json({ error: "출근부 데이터 업데이트에 실패하였습니다" });
+  }
+};
+
+// // 출근부 작성 (CSV 파일 업로드 및 데이터베이스 저장) -----------------------------------------------------------------------------------------------------------
+// const writeAttendance = async (req, res) => {
+//   try {
+//     if (!req.file) {
+//       return res.status(400).json({ error: "업로드된 파일이 없습니다" });
+//     }
+//     // 다음 경로에 csv파일 저장 및 원본파일 이름 저장
+//     const originalFileName = req.file.originalname;
+//     const uploadDir = path.join(__dirname, "../../../uploads/officeHourFile");
+//     // 폴더가 없으면 생성
+//     if (!fs.existsSync(uploadDir)) {
+//       fs.mkdirSync(uploadDir, { recursive: true });
+//     }
+
+//     const filePath = path.join(uploadDir, originalFileName);
+//     fs.writeFileSync(filePath, req.file.buffer);
+
+//     const jsonArray = [];
+//     // 파일을 읽고 인코딩을 EUC-KR에서 UTF-8로 변환하여 CSV 파싱
+//     fs.createReadStream(filePath)
+//       // EUC-KR 인코딩에서 UTF-8로 변환
+//       .pipe(iconv.decodeStream("euc-kr"))
+//       .pipe(
+//         csv({
+//           // CSV 파일의 헤더를 매핑하여 올바르게 인식하도록 설정
+//           mapHeaders: ({ header }) => {
+//             switch (header) {
+//               case "占쌩삼옙占쏙옙占쏙옙":
+//                 return "발생일자";
+//               case "占쌩삼옙占시곤옙":
+//                 return "발생시각";
+//               case "占쌤몌옙占쏙옙ID":
+//                 return "단말기ID";
+//               case "占쏙옙占쏙옙占폠D":
+//                 return "사용자ID";
+//               case "占싱몌옙":
+//                 return "이름";
+//               case "占쏙옙占쏙옙占싫�":
+//                 return "사원번호";
+//               case "占쏙옙占쏙옙":
+//                 return "직급";
+//               case "占쏙옙占�":
+//                 return "구분";
+//               default:
+//                 return header;
+//             }
+//           },
+//         })
+//       )
+//       .on("data", (row) => {
+//         // 발생일자와 발생시각 필드가 있는 경우에만 데이터를 추가
+//         if (row["발생일자"] && row["발생시각"]) {
+//           jsonArray.push({
+//             occurrenceDate: row["발생일자"].trim(),
+//             occurrenceTime: row["발생시각"].trim(),
+//             terminalId: parseInt(row["단말기ID"]) || 0,
+//             userId: parseInt(row["사용자ID"]) || 0,
+//             name: row["이름"] ? row["이름"].trim() : null,
+//             employeeNumber: row["사원번호"] ? row["사원번호"].trim() : null,
+//             position: row["직급"] ? row["직급"].trim() : null,
+//             category: row["구분"] ? row["구분"].trim() : null,
+//             mode: row["모드"] ? row["모드"].trim() : null,
+//             authentication: row["인증"] ? row["인증"].trim() : null,
+//             result: row["결과"] ? row["결과"].trim() : null,
+//           });
+//         } else {
+//           console.error(
+//             "필수 필드(occurrenceDate, occurrenceTime)가 누락된 행 발생:",
+//             row
+//           );
+//         }
+//       })
+//       .on("end", async () => {
+//         try {
+//           for (const record of jsonArray) {
+//             if (record.occurrenceDate && record.occurrenceTime) {
+//               await Attendance.create(record);
+//             }
+//           }
+//           res
+//             .status(200)
+//             .json({ message: "출근부 데이터가 성공적으로 업로드되었습니다" });
+//         } catch (error) {
+//           console.error("DB 저장 중 오류 발생:", error);
+//           res
+//             .status(400)
+//             .json({ error: "데이터 저장 중 오류가 발생했습니다." });
+//         }
+//       })
+//       .on("error", (error) => {
+//         console.error("CSV 파싱 중 오류 발생:", error);
+//         res
+//           .status(500)
+//           .json({ error: "출근부 데이터 업로드에 실패하였습니다" });
+//       });
+//   } catch (error) {
+//     console.error("출근부 작성 중 오류 발생:", error);
+//     res.status(500).json({ error: "출근부 데이터 업로드에 실패하였습니다" });
+//   }
+// };
+
+// 출근부 작성 (CSV 파일 업로드 및 데이터베이스 저장) -----------------------------------------------------------------------------------------------------------
 const writeAttendance = async (req, res) => {
   try {
     if (!req.file) {
       return res.status(400).json({ error: "업로드된 파일이 없습니다" });
     }
-    // 다음 경로에 csv파일 저장 및 원본파일 이름 저장
+
+    // 파일 저장 경로 설정
     const originalFileName = req.file.originalname;
     const uploadDir = path.join(__dirname, "../../../uploads/officeHourFile");
+
     // 폴더가 없으면 생성
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir, { recursive: true });
@@ -33,84 +171,75 @@ const writeAttendance = async (req, res) => {
     const filePath = path.join(uploadDir, originalFileName);
     fs.writeFileSync(filePath, req.file.buffer);
 
-    const jsonArray = [];
-    // 파일을 읽고 인코딩을 EUC-KR에서 UTF-8로 변환하여 CSV 파싱
-    fs.createReadStream(filePath)
-      // EUC-KR 인코딩에서 UTF-8로 변환
-      .pipe(iconv.decodeStream("euc-kr"))
-      .pipe(
-        csv({
-          // CSV 파일의 헤더를 매핑하여 올바르게 인식하도록 설정
-          mapHeaders: ({ header }) => {
-            switch (header) {
-              case "占쌩삼옙占쏙옙占쏙옙":
-                return "발생일자";
-              case "占쌩삼옙占시곤옙":
-                return "발생시각";
-              case "占쌤몌옙占쏙옙ID":
-                return "단말기ID";
-              case "占쏙옙占쏙옙占폠D":
-                return "사용자ID";
-              case "占싱몌옙":
-                return "이름";
-              case "占쏙옙占쏙옙占싫�":
-                return "사원번호";
-              case "占쏙옙占쏙옙":
-                return "직급";
-              case "占쏙옙占�":
-                return "구분";
-              default:
-                return header;
-            }
-          },
-        })
-      )
-      .on("data", (row) => {
-        // 발생일자와 발생시각 필드가 있는 경우에만 데이터를 추가
-        if (row["발생일자"] && row["발생시각"]) {
-          jsonArray.push({
-            occurrenceDate: row["발생일자"].trim(),
-            occurrenceTime: row["발생시각"].trim(),
-            terminalId: parseInt(row["단말기ID"]) || 0,
-            userId: parseInt(row["사용자ID"]) || 0,
-            name: row["이름"] ? row["이름"].trim() : null,
-            employeeNumber: row["사원번호"] ? row["사원번호"].trim() : null,
-            position: row["직급"] ? row["직급"].trim() : null,
-            category: row["구분"] ? row["구분"].trim() : null,
-            mode: row["모드"] ? row["모드"].trim() : null,
-            authentication: row["인증"] ? row["인증"].trim() : null,
-            result: row["결과"] ? row["결과"].trim() : null,
-          });
-        } else {
-          console.error(
-            "필수 필드(occurrenceDate, occurrenceTime)가 누락된 행 발생:",
-            row
-          );
+    // 엑셀 파일 읽기
+    const workbook = xlsx.readFile(filePath);
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+
+    const jsonArray = xlsx.utils.sheet_to_json(worksheet, {
+      raw: true,
+      defval: null,
+      header: 1
+    });
+
+    // 데이터 가공
+    const headers = jsonArray[0];
+    const dataRows = jsonArray.slice(1);
+
+    const formattedData = dataRows.reduce((acc, row) => {
+      const rowData = {};
+      headers.forEach((header, index) => {
+        switch (header) {
+          case "이름":
+            rowData.username = row[index] ? row[index].trim() : null;
+            break;
+          case "발생일자":
+            rowData.Date = row[index] ? row[index].trim() : null;
+            break;
+          case "발생시각":
+            rowData.Time = row[index] ? row[index].trim() : null;
+            break;
+          case "모드":
+            rowData.Mode = row[index] ? row[index].trim() : null;
+            break;
         }
-      })
-      .on("end", async () => {
-        try {
-          for (const record of jsonArray) {
-            if (record.occurrenceDate && record.occurrenceTime) {
-              await Attendance.create(record);
-            }
-          }
-          res
-            .status(200)
-            .json({ message: "출근부 데이터가 성공적으로 업로드되었습니다" });
-        } catch (error) {
-          console.error("DB 저장 중 오류 발생:", error);
-          res
-            .status(400)
-            .json({ error: "데이터 저장 중 오류가 발생했습니다." });
-        }
-      })
-      .on("error", (error) => {
-        console.error("CSV 파싱 중 오류 발생:", error);
-        res
-          .status(500)
-          .json({ error: "출근부 데이터 업로드에 실패하였습니다" });
       });
+      
+      if (rowData.username && rowData.Date && rowData.Time && rowData.Mode) {
+        const key = `${rowData.username}-${rowData.Date}`;
+        if (!acc[key]) {
+          acc[key] = {
+            username: rowData.username,
+            Date: rowData.Date,
+            DataList: { 출근시간: null, 퇴근시간: null,  구분: null}
+          };
+        }
+        
+        if (rowData.Mode === "출근") {
+          acc[key].DataList.출근시간 = rowData.Time;
+        } else if (rowData.Mode === "퇴근") {
+          acc[key].DataList.퇴근시간 = rowData.Time;
+        }else if (rowData.Mode === "구분") {
+          acc[key].DataList.구분 = rowData.Time;
+        }
+      }
+      return acc;
+    }, {});
+
+    try {
+      for (const key in formattedData) {
+        const record = formattedData[key];
+        await Attendance.create({
+          username: record.username,
+          Date: record.Date,
+          DataList: [record.DataList.출근시간, record.DataList.퇴근시간, record.DataList.구분]
+        });
+      }
+      res.status(200).json({ message: "출근부 데이터가 성공적으로 업로드되었습니다" });
+    } catch (error) {
+      console.error("DB 저장 중 오류 발생:", error);
+      res.status(400).json({ error: "데이터 저장 중 오류가 발생했습니다." });
+    }
   } catch (error) {
     console.error("출근부 작성 중 오류 발생:", error);
     res.status(500).json({ error: "출근부 데이터 업로드에 실패하였습니다" });
@@ -120,4 +249,5 @@ const writeAttendance = async (req, res) => {
 module.exports = {
   writeAttendance,
   getAllAttendance,
+  updateAttendance
 };
--- backend/lib/models/attendance/officeHour.js
+++ backend/lib/models/attendance/officeHour.js
@@ -1,59 +1,86 @@
+// module.exports = (sequelize, DataTypes) => {
+//     const Attendance = sequelize.define(
+//       "Attendance",
+//       {
+//         // 발생 날짜
+//         occurrenceDate: {
+//           type: DataTypes.DATE,
+//           allowNull: true,
+//         },
+//         // 발생시간
+//         occurrenceTime: {
+//           type: DataTypes.TIME,
+//           allowNull: true,
+//         },
+//         // 단말기 ID
+//         terminalId: {
+//           type: DataTypes.INTEGER,
+//           allowNull: true,
+//         },
+//         // 사용자 아이디
+//         userId: {
+//           type: DataTypes.INTEGER,
+//           allowNull: true,
+//         },
+//         name: {
+//           type: DataTypes.STRING,
+//           allowNull: true,
+//         },
+//         // 사원번호
+//         employeeNumber: {
+//           type: DataTypes.STRING,
+//           allowNull: true,
+//         },
+//         // 직급
+//         position: {
+//           type: DataTypes.STRING,
+//           allowNull: true,
+//         },
+//         // 구분
+//         category: {
+//           type: DataTypes.STRING,
+//           allowNull: true,
+//         },
+//         // 모드 (출, 퇴근)
+//         mode: {
+//           type: DataTypes.STRING,
+//           allowNull: true,
+//         },
+//         // 인증사항
+//         authentication: {
+//           type: DataTypes.STRING,
+//           allowNull: true,
+//         },
+//         // 결과
+//         result: {
+//           type: DataTypes.STRING,
+//           allowNull: true,
+//         },
+//       },
+//       {
+//         tableName: "attendance",
+//         timestamps: true,
+//       }
+//     );
+  
+//     return Attendance;
+//   };
+  
+
 module.exports = (sequelize, DataTypes) => {
     const Attendance = sequelize.define(
       "Attendance",
       {
-        // 발생 날짜
-        occurrenceDate: {
-          type: DataTypes.DATE,
-          allowNull: true,
-        },
-        // 발생시간
-        occurrenceTime: {
-          type: DataTypes.TIME,
-          allowNull: true,
-        },
-        // 단말기 ID
-        terminalId: {
-          type: DataTypes.INTEGER,
-          allowNull: true,
-        },
-        // 사용자 아이디
-        userId: {
-          type: DataTypes.INTEGER,
-          allowNull: true,
-        },
-        name: {
+        username: {
           type: DataTypes.STRING,
-          allowNull: true,
+          allowNull: false,
         },
-        // 사원번호
-        employeeNumber: {
+        Date: {
           type: DataTypes.STRING,
-          allowNull: true,
+          allowNull: false,
         },
-        // 직급
-        position: {
-          type: DataTypes.STRING,
-          allowNull: true,
-        },
-        // 구분
-        category: {
-          type: DataTypes.STRING,
-          allowNull: true,
-        },
-        // 모드 (출, 퇴근)
-        mode: {
-          type: DataTypes.STRING,
-          allowNull: true,
-        },
-        // 인증사항
-        authentication: {
-          type: DataTypes.STRING,
-          allowNull: true,
-        },
-        // 결과
-        result: {
-          type: DataTypes.STRING,
+        DataList: {
+          type: DataTypes.JSON, // 배열 형태로 저장
           allowNull: true,
         },
       },
