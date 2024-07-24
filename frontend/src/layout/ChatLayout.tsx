50fcff64641cd046b5292ffcec8d5f9269c0ac55
HYEONJI1209 <hyeonji1209@naver.com>
2024-07-24 16:58:09 +0900

    메신저 퍼블리싱 완료

--- frontend/src/layout/ChatLayout.tsx
+++ frontend/src/layout/ChatLayout.tsx
@@ -1,17 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Header, MessageSidebar } from "../components";
-import { useRecoilState } from 'recoil';
-import { isSidebarVisibleState } from '../recoil/atoms';
 
 const ChatLayout = () => {
-    const [isSidebarVisible] = useRecoilState(isSidebarVisibleState);
 
     return (
         <main className="page-wrapper">
             <Header />
 
-            <div className="content-wrapper">
-                {isSidebarVisible && <MessageSidebar />}
+            <div className="content-wrapper" style={{ display: 'flex' }}>
+                <MessageSidebar />
                 <Outlet />
             </div>
         </main>
