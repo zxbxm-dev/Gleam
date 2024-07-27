1c4d27164f4abd86301fcc1b6a73e48261d70a60
HYEONJI1209 <108385746+HYEONJI1209@users.noreply.github.com>
2024-07-27 19:03:39 +0900

    진행중

--- frontend/src/layout/BaseLayout.tsx
+++ frontend/src/layout/BaseLayout.tsx
@@ -1,12 +1,28 @@
+import { useEffect } from 'react';
 import { Outlet } from "react-router-dom";
 import { Header, Sidebar } from "../components";
 import { useRecoilState } from 'recoil';
 import { isSidebarVisibleState, isHrSidebarVisibleState } from '../recoil/atoms';
 
 const BaseLayout = () => {
-  const [isSidebarVisible] = useRecoilState(isSidebarVisibleState);
+  const [isSidebarVisible, setIsSidebarVisible] = useRecoilState(isSidebarVisibleState);
   const [isHrSidebarVisible] = useRecoilState(isHrSidebarVisibleState);
 
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 1024px)');
+
+    const handleMediaQueryChange = (event:any) => {
+      setIsSidebarVisible(!event.matches);
+    };
+
+    handleMediaQueryChange(mediaQuery);
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, [setIsSidebarVisible]);
 
   let marginLeft = 0;
   if (isSidebarVisible) {
