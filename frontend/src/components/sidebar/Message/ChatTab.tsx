d878b2607dcb8d98a1490737d9277fa73ebcd9ed
Johnsnim <qw506799@naver.com>
2024-07-31 15:05:23 +0900

    대화방 프로필 설정 커스텀 모달

--- frontend/src/components/sidebar/Message/ChatTab.tsx
+++ frontend/src/components/sidebar/Message/ChatTab.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   MessageMe,
   MessageMenu,
@@ -10,6 +10,7 @@ import {
   PopoverTrigger,
   Portal,
 } from "@chakra-ui/react";
+import CustomModal from "../../modal/CustomModal";
 
 interface ChatDataTabProps {
   dummyData: any[];
@@ -35,6 +36,40 @@ const ChatDataTab: React.FC<ChatDataTabProps> = ({
   onPersonClick,
   userPosition,
 }) => {
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [paletteIndex, setPaletteIndex] = useState<number | null>(null);
+
+  const ProfilePalette: string[] = [
+    "#FF96EE",
+    "#FF9191",
+    "#F3C639",
+    "#91C06B",
+    "#45995C",
+    "#6AC9FF",
+    "#618DFF",
+    "#916AFF",
+    "#5C6174",
+    "#6A5644",
+  ];
+
+  const ProfileBorderPalette: string[] = [
+    "#EC64D6",
+    "#E85D5D",
+    "#E3B337",
+    "#6FA742",
+    "#3B814E",
+    "#35A7E7",
+    "#366AEF",
+    "#7248E9",
+    "#404558",
+    "#59422D",
+  ];
+
+  const [profileName, setProfileName] = useState<string>("");
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProfileName(e.target.value);
+  };
   return (
     <div className="chat-data-tab">
       <li
@@ -123,7 +158,12 @@ const ChatDataTab: React.FC<ChatDataTabProps> = ({
                   _focus={{ boxShadow: "none" }}
                 >
                   <div className={`Message-OnClick-Menu`}>
-                    <div className="ProfileChange">대화방 프로필 설정</div>
+                    <div
+                      className="ProfileChange"
+                      onClick={() => setOpenModal(true)}
+                    >
+                      대화방 프로필 설정
+                    </div>
                     <div className="OutOfChat">대화 나가기</div>
                   </div>
                 </PopoverContent>
@@ -131,6 +171,68 @@ const ChatDataTab: React.FC<ChatDataTabProps> = ({
             </div>
           </Popover>
         ))}
+
+      <CustomModal
+        isOpen={openModal}
+        onClose={() => setOpenModal(false)}
+        header={"대화방 프로필 설정"}
+        headerTextColor="white"
+        footer1={"수정"}
+        footer1Class="green-btn"
+        //onFooter1Click={handleSubmit}
+        footer2={"취소"}
+        footer2Class="gray-btn"
+        onFooter2Click={() => setOpenModal(false)}
+        width="240px"
+        height="auto"
+      >
+        <div className="body-container ProfileSetting">
+          <div className="ProfileSetting">
+            <div className="ProfileAndPalette">
+              <div
+                className="Profile"
+                style={{
+                  backgroundColor: paletteIndex
+                    ? ProfilePalette[paletteIndex]
+                    : "",
+                  border: paletteIndex
+                    ? `1px solid ${ProfileBorderPalette[paletteIndex]}`
+                    : "",
+                }}
+              >
+                {profileName ? profileName.slice(0, 1) : ""}
+              </div>
+              <div className="Palette">
+                {ProfilePalette.map((color, index) => (
+                  <div
+                    style={{
+                      backgroundColor: color,
+                      border: `1px solid ${ProfileBorderPalette[index]}`,
+                    }}
+                    onClick={() => {
+                      setPaletteIndex(index);
+                    }}
+                  ></div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="ProfileSetting">
+            <input
+              placeholder="(필수) 대화방 이름을 입력해주세요."
+              className="TextInputCon"
+              onChange={handleNameChange}
+            />
+          </div>
+          <div className="ProfileSetting">
+            <textarea
+              placeholder="(선택) 설명을 입력해주세요."
+              className="TextInputCon large"
+              //onChange={handleTitleChange}
+            />
+          </div>
+        </div>
+      </CustomModal>
     </div>
   );
 };
