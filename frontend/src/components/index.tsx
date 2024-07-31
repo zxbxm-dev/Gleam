b031fe04989f73b7e30384e41148a6263dcb2951
minseok9 <minseok7746@naver.com>
2024-07-31 11:14:33 +0900

    헤더, 메세지 헤더 추가

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as Sidebar } from "./sidebar/Sidebar";
 export { default as MemberSidebar } from "./sidebar/MemberSidebar";
 export { default as MessageSidebar } from "./sidebar/Message/MessageSidebar";
 export { default as Header } from "./header/Header";
+export { default as MessageHeader } from "./header/MessageHeader";
