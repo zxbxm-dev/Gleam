2888e30bcaf55605e4138c27d7285b2e33c92fcd
zxbxm-dev <zxbxm.dev@gmail.com>
2024-08-13 11:21:06 +0900

    이메일 데이터베이스 모델 추가

---
+++ backend/lib/models/email/emailAction.js
@@ -0,0 +1,49 @@
+module.export = (sequelize, DataTypes) => {
+    const emailAction = sequelize.define(
+        "emailAction",
+        {   
+            //PK
+            Id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            //email 테이블 emailId 외래키 설정
+            emailId:{
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references:{
+                    model: 'email',
+                    key: "Id",
+                },
+                ondelete: 'CASCADE',
+                onupdate: 'CASCADE'
+            },
+            //user테이블 userId 외래키 설정
+            userId: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                references:{
+                    model : 'user',
+                    key: 'userId',
+                },
+                ondelete: 'CASCADE',
+                onupdate: 'CASCADE'
+            },
+            action: {
+                type: DataTypes.ENUM,
+                values: ['sent','sent_cancled','opened','deleted','cancled','registJunk'],
+                allowNull: false,
+            },
+            actionTimeStamp: {
+                type: DataTypes.DATE,
+                defaultValue: DataTypes.NOW
+            },
+        },
+        {
+            tableName: "emailAction"
+        }
+    );
+    return emailAction;
+}
