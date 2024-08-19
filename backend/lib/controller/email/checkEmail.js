31b06486170d1a879c4a589531f46b6c6998787f
zxbxm-dev <zxbxm.dev@gmail.com>
2024-08-19 16:46:13 +0900

    이메일기능 -IMAP 설정 코드 추가 -model 코드 수정 -router, controller 코드 추가

---
+++ backend/lib/controller/email/checkEmail.js
@@ -0,0 +1,16 @@
+const models = require("../../models");
+const email = models.Email;
+
+//이메일 조회하기
+const getAllEmail = async (req, res) => {
+    try{
+        const emails = await email.findAll();
+        res.status(200).json("이메일 조회를 완료했습니다:",emails);
+    }catch(error){
+        console.error("이메일 조회 중 오류가 발생했습니다.: ", error);
+        res.status(500).json({ error: "이메일 조회에 실패했습니다." });
+    }
+};
+module.exports = {
+    getAllEmail,
+}
