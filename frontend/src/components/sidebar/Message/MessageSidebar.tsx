38ecb808f7b7e5589288eb8a117167a98b70f3da
HYEONJI1209 <hyeonji1209@naver.com>
2024-08-22 14:41:07 +0900

    socket 리소스 문제 해결

--- frontend/src/components/sidebar/Message/MessageSidebar.tsx
+++ frontend/src/components/sidebar/Message/MessageSidebar.tsx
@@ -65,7 +65,6 @@ const MessageSidebar: React.FC = () => {
   const [borderColor, setBorderColor] = useState<string>("");
   const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
   const setUserStatetoServer = useSetRecoilState(userStateMessage);
-  const socket = io('http://localhost:3001', { transports: ["websocket"] });
   const location = useLocation();
 
   useEffect(() => {
@@ -86,38 +85,34 @@ const MessageSidebar: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    const userId = user.userID;
+    const socket = io('http://localhost:3001', {
+      transports: ['websocket'],
+    });
 
-    // console.log(`[Client] Connecting to socket server at http://localhost:3001`);
-
-    // 서버와의 소켓 연결이 성공했는지 확인
     socket.on('connect', () => {
-      // console.log(`[Client] Connected to socket server with id: ${socket.id}`);
+      console.log(`[Client] Connected to socket server with id: ${socket.id}`);
     });
 
-    // 서버에 채팅방 목록 요청
-    // console.log(`[Client] Requesting chat rooms for userId: ${userId}`);
-    socket.emit('getChatRooms', userId);
+    socket.on('connect_error', (error) => {
+      console.error('Connection error:', error);
+    });
 
-    // 서버로부터 채팅방 목록을 받아옴
     socket.on('chatRooms', (data: any) => {
-      // console.log('[Client] Received chat rooms:', data);
-      setChatRooms(data);
       setChatRooms(data);
     });
 
-    // 서버와의 연결이 끊겼을 때
     socket.on('disconnect', () => {
-      // console.log('[Client] Disconnected from socket server');
+      console.log('[Client] Disconnected from socket server');
     });
 
-    // 컴포넌트 언마운트 시 소켓 연결 해제
     return () => {
       socket.off('chatRooms');
       socket.off('connect');
       socket.off('disconnect');
+      socket.close();
     };
   }, [user.userID]);
+
 
   const toggleDepartmentExpansion = (departmentName: string) => {
     setExpandedDepartments((prevExpandedDepartments) => ({
@@ -256,7 +251,7 @@ const MessageSidebar: React.FC = () => {
     };
   }, [timeoutId, location.pathname]);
 
-  
+
   // 렉이 너무 많이 걸려서 주석처리합니다! -- socket 활동 상태
   // useEffect(() => {
   //   console.log("Emitting user status to server:", {
