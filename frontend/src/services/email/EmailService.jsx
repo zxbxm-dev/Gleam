af43b04ec3c540e6d2ce32aa48dace2b959cabfb
minseok9 <minseok7746@naver.com>
2024-09-05 11:06:29 +0900

    메일: 스팸 설정 기능

--- frontend/src/services/email/EmailService.jsx
+++ frontend/src/services/email/EmailService.jsx
@@ -7,41 +7,54 @@ const SendMail = (formData) => {
       'Content-Type': 'multipart/form-data'
     }
   });
-}
+};
 
 // 메일 조회
 const CheckEmail = (userId) => {
   return api.get(`checkEmail/${userId}`);
-}
+};
 
 // 예약 취소
 const cancleQueueEmail = (Id) => {
   return api.delete(`/cancleQueueEmail/${Id}`);
-}
+};
 
 // 메일 삭제
 const DeleteEmail = (mailId, messageId) => {
   return api.delete(`/deleteEmail/${mailId}/${messageId}`);
-}
+};
 
 // 메일 임시 저장
 const DraftEmail = (formData) => {
   return api.post("/draftEmail", formData);
-}
+};
 
 // 중요 메일 등록
 const StarringEmail = (formData) => {
   return api.put("/starringEmail", formData);
-}
+};
 
 // 스팸 메일 등록
-const JunkEmail = (formData) => {
-  return api.put("/registerJunk", formData);
-}
+const JunkEmail = (Id) => {
+  return api.put(`/registerJunk/${Id}`);
+};
 
 // 스팸 유저 등록
 const AddJunkList = (formData) => {
   return api.post("/addJunkList", formData);
-}
+};
 
-export { SendMail, CheckEmail, cancleQueueEmail, DeleteEmail, DraftEmail, StarringEmail, JunkEmail, AddJunkList };
+// 스팸 유저 확인
+const CheckJunkList = (params) => {
+  const queryString = `?userId=${params.userId}`;
+  return api.get("/getJunkList" + queryString);
+};
+
+// 스팸 유저 삭제
+const RemoveJunkList = (junkId, userId) => {
+  return api.delete(`/removeJunklist/${junkId}`, {
+    data: { userId }
+  });
+};
+
+export { SendMail, CheckEmail, cancleQueueEmail, DeleteEmail, DraftEmail, StarringEmail, JunkEmail, AddJunkList, CheckJunkList, RemoveJunkList };
