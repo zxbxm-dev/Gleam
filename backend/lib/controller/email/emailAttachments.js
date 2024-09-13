61ceaa4f6d93c77f13a57ae7ec96da252ea50dc3
zxbxm-dev <zxbxm.dev@gmail.com>
2024-09-13 11:00:26 +0900

    첨부파일 저장 코드 수정

--- backend/lib/controller/email/emailAttachments.js
+++ backend/lib/controller/email/emailAttachments.js
@@ -7,7 +7,7 @@ async function getAttachmentsByEmailId(Id){
     try{
         const attachments = await models.EmailAttachment.findAll({
             where:{
-                Id: Id 
+                emailId: emailId 
             }
         });
         return attachments;
