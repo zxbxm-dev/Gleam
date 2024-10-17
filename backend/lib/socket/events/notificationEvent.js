0ada7764608f9a8ee76fc8b62e133926fce6d8d8
zxbxm-dev <zabam.dev@gmail.com>
2024-10-17 11:07:17 +0900

    클라이언트에 채팅방 사용자 정보 요청 -notificationEvent 클라이언트에 사용자 정보 요청 - notificationHandler 받아온 사용자 정보로 newMsg 함수에 적용

--- backend/lib/socket/events/notificationEvent.js
+++ backend/lib/socket/events/notificationEvent.js
@@ -8,6 +8,20 @@ module.exports = (io, socket) => {
     }
 };
 
+//메세지의 sender와 receiver 의 정보를 요청하는 함수
+socket.on("requestUserData", async (roomId, sender,receiver) => {
+    try{
+        if(!roomId) {
+            throw new Error ("채팅방 ID가 제공되지 않았습니다.");
+        }
+
+        await notificationHandlers.requestUserData(socket,roomId,sender,receiver);
+
+    }catch(error){
+        console.error("메세지 수신 , 발신 사용자 정보 요청 처리 중 에러 발생")
+    }
+})
+
 //사용자에게 새로운 메세지가 전달됐을 때 
 socket.on("getNewMsg",async (userId) => {
     try{
@@ -18,7 +32,7 @@ socket.on("getNewMsg",async (userId) => {
         await notificationHandlers.getNewMsg(socket,userId)
 
     }catch(error) {
-        console.error("메세지알림 요청 처리 중 발생 : ", error);
+        console.error("메세지알림 요청 처리 중 에러 발생 : ", error);
         socket.emit("error", { message: "메세지알림을 가져오는 중 오류가 발생했습니다."});
     }
 });
