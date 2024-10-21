a9083e640c6422d24817540b6b0755bec84cc9a9
zxbxm-dev <zabam.dev@gmail.com>
2024-10-21 15:20:30 +0900

    메신저 알림 기능 수정 - 네임스페이스 주석 처리 - sendMessageToRoomParticipants 함수에 socket 인자 추가 - notificationHandler 코드 수정

--- backend/lib/socket/handlers/notificationHandlers.js
+++ backend/lib/socket/handlers/notificationHandlers.js
@@ -1,13 +1,12 @@
+// const socket = require("..");
 const models = require("../../models");
 const { Message } = models;
-const { countUnreadMessages } = require("../handlers/statusHandlers");
 
 //새로운 메세지가 있을 때
 const getNewMsg = async (socket, messageData) => {
     try {
       const messageId = messageData.messageId;
       const receiver = messageData.receiverId;
-      const roomId = messageData.roomId;
 
       const newMsg = await Message.findAll({
       where:{
@@ -16,18 +15,17 @@ const getNewMsg = async (socket, messageData) => {
       },
     });
 
-    //안읽은 메세지 확인하는 함수
-      const unreadMsg = await countUnreadMessages(socket, receiver, roomId );
-
-      if (newMsg && unreadMsg) {
+      if (newMsg) {
         socket.emit("newMsgNoti", {
-          message: `새로운 메세지가 ${unreadMsg}개 있습니다.`,
-          messages: newMsg 
+          message: `새로운 메세지가 있습니다.`,
+          messages: newMsg          
         });
       } else {
         console.log("새로운 메시지가 없습니다.");
       }
+
       return newMsg;
+    
     } catch (error) {
       socket.emit("error", {
         message: "새로운 메세지 알림 처리 중 오류 발생",
