86e9740cc88b207643784a5285b756d75ac23d23
zxbxm-dev <zabam.dev@gmail.com>
2024-11-04 14:33:24 +0900

    getNewMsg 에 messageData 데이터 추가

--- backend/lib/socket/handlers/notificationHandlers.js
+++ backend/lib/socket/handlers/notificationHandlers.js
@@ -5,40 +5,17 @@ const { Message } = models;
 //새로운 메세지가 있을 때
 const getNewMsg = async (socket, messageData, connectedUsers) => {
     try {
-
+      const receiverSocketId = connectedUsers[messageData.receiverId];
       //수신자의 온라인 여부를 확인하는 코드
       if(Object.keys(connectedUsers).includes(messageData.receiverId)){
-        const receiverSocketId = connectedUsers[messageData.receiverId];
         await onlineUser( receiverSocketId, messageData);
       }else(
-        await offlineUser(socket, messageData)
+        await offlineUser(receiverSocketId, messageData)
       );
 
-      //수신자의 
-
-      //새로운 메세지 알람 
-      const messageId = messageData.messageId;
-      const receiver = messageData.receiverId;
-
-      const newMsg = await Message.findAll({
-      where:{
-        messageId : messageId,
-        receiverId : receiver,
-      },
-    });
-      // if (newMsg) {
-      //   socket.emit("newMsgNoti", {
-      //     message: `새로운 메세지가 있습니다.`,
-      //     messages: newMsg          
-      //   });
-      // } else {
-      //   console.log("새로운 메시지가 없습니다.");
-      // }
-      return newMsg;
-    
     } catch (error) {
       socket.emit("error", {
-        message: "새로운 메세지 알림 처리 중 오류 발생",
+        message: "수신자 온라인 여부 확인 처리 중 오류 발생",
         details: error.message,
       });
     }
@@ -71,22 +48,6 @@ const offlineUser = async (socket, messageData) => {
   }
 
 };
-
-
-//사용자가 특정 방에 접속중인지 확인 
-const checkUserConnected = async (socket, roomId, connectedUsers, messageData ) => {
-
-  const checkRoomsForSocket = (socket) => {
-    console.log(`Socket ID ${socket.id} is in rooms:`, Array.from(socket.rooms));
-  };
-  
-  if(roomId == messageData.roomId){
-
-  }else{
-
-  }
-    
-}
 
 module.exports = {
     getNewMsg,
