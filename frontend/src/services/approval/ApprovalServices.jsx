a8fc4b1100e13aaacad578d9ea699615d47c528c
tource <tource20@naver.com>
2024-12-16 15:57:03 +0900

    feat: 의견작성, 반려 라우터 추가

--- frontend/src/services/approval/ApprovalServices.jsx
+++ frontend/src/services/approval/ApprovalServices.jsx
@@ -9,52 +9,68 @@ const getMyReports = (params) => {
 // 결재할 문서 목록 조회
 const getDocumentsToApprove = (params) => {
   return api.get("/getDocumentsToApprove", { params });
-}
+};
 
 // 결재 진행 중인 문서 목록 조회
 const getDocumentsInProgress = (params) => {
   return api.get("/getDocumentsInProgress", { params });
-}
+};
 
 // 반려된 문서 목록 조회
 const getRejectedDocuments = (params) => {
   return api.get("/getRejectedDocuments", { params });
-}
+};
 
 // 결재 완료된 문서 목록 조회
 const getApprovedDocuments = (params) => {
   return api.get("/getApprovedDocuments", { params });
-}
+};
 
 // 보고서 의견 작성하기
 const WriteApprovalOp = (report_id, formData) => {
   return api.post(`/writeApprovalOp/${report_id}`, formData);
-}
+};
 
 // 보고서 반려 작성하기
 const WriteApproval = (report_id, formData) => {
   return api.post(`/writeApproval/${report_id}`, formData);
-}
+};
 
 // 결재하기
 const HandleApproval = (report_id, formData) => {
   return api.post(`/handleApproval/${report_id}`, formData);
-}
+};
 
 // 보고서 상세 조회
 const CheckReport = (report_id) => {
   return api.get(`/checkReport/${report_id}`, {
-    responseType: 'blob',
+    responseType: "blob",
     headers: {
-      'Content-Type': 'application/pdf',
-    }
+      "Content-Type": "application/pdf",
+    },
   });
+};
+
+// 의견내용 반려 내용 조회
+const getReportOpinion = (report_id) => {
+  return api.get(`/getReportOpinions/${report_id}`);
 };
 
 // 보고서 삭제
 const DeleteReport = (report_id) => {
   return api.delete(`/deleteReport/${report_id}`);
-}
+};
 
-
-export { getMyReports, getDocumentsToApprove, getDocumentsInProgress, getRejectedDocuments, getApprovedDocuments, HandleApproval, WriteApprovalOp, WriteApproval, CheckReport, DeleteReport };
+export {
+  getMyReports,
+  getDocumentsToApprove,
+  getDocumentsInProgress,
+  getRejectedDocuments,
+  getApprovedDocuments,
+  HandleApproval,
+  WriteApprovalOp,
+  WriteApproval,
+  CheckReport,
+  DeleteReport,
+  getReportOpinion,
+};
